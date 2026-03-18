841ee0198726e2c59804d6b4a255ef6007de75ce
Paula Artusso <paula.artusso@ad.neteco.org>
2026-03-18 10:40:34 +0100

    remove secrets from environment

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -3,8 +3,8 @@ export const environment = {
   azureFunctions: {
     baseUrl: 'https://masterbuilderbackend.azurewebsites.net/api',
      keys: {
-        GetProjects: 'DaLPqurl7qprFslGmheKaZ4qPCTt8L1Wqy1BReTKNxMtAzFuJCfq7w==',
-        GetProjectById: 'xYAAuTceC93rMOtI2TIc1h123470bV729EDIjGPEEcEiAzFuBVxusg==',
+        GetProjects: '',
+  GetProjectById: '',
     }
   }
 };
