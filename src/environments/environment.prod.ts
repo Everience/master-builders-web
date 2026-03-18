6c357353f2803d01e7fbe12ec3f39634c5fe6b2b
Paula Artusso <paula.artusso@ad.neteco.org>
2026-03-18 12:06:36 +0100

    environment files hostKey regeneration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,6 @@ export const environment = {
   production: true,
   azureFunctions: {
     baseUrl: 'https://masterbuilderbackend.azurewebsites.net/api',
-    keys: {
-      GetProjects: '',
-      GetProjectById: '',
-    }
+    hostKey: ''
   }
 };
