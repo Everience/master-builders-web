7f33efe1ff2219d61bba41c57336aa184e5f6f79
Paula Artusso <paula.artusso@ad.neteco.org>
2026-04-29 12:17:47 +0200

    power bi no extraction option added for normal users

--- src/app/bi-test/bi-test.component.ts
+++ src/app/bi-test/bi-test.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/project/auth.service';
+import { MsalService } from '@azure/msal-angular';
 
 @Component({
   selector: 'app-bi-test',
@@ -8,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrl: './bi-test.component.scss'
 })
 export class BiTestComponent {
+isAdmin = false;
+  userName = '';
+  acc: any;
 
+  constructor(private authService: AuthService, private msalService: MsalService) {}
+
+  ngOnInit() {
+    this.authService.isAdmin().subscribe(result => {
+      this.isAdmin = result;
+    });
+    this.userName = this.authService.getUserName();
+    this.acc = this.authService.getAccount();
+    console.log(this.msalService.instance.getActiveAccount()?.idTokenClaims, this.acc);
+  }
 }
