410d10e9e9b8e17434fff874f610b11a1f80fb32
Paula Artusso <paula.artusso@ad.neteco.org>
2026-05-07 17:15:59 +0200

    remove sensitive config from frontend

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,6 +2,12 @@ export const environment = {
   production: false,
   azureFunctions: {
     baseUrl: 'https://masterbuilderbackend.azurewebsites.net/api',
-    hostKey: 'mxIPLR2WP2oC951-hqHniBjJ43kVtLbrfz5Jt5Z03jhWAzFu7vbb5A=='
+    //baseUrl: '/api',
+  },
+  msal: {
+    clientId: '767114fd-3d06-45b5-a2bb-7709837a92d9',
+    tenantId: '456c3ca4-0074-406c-aefc-7ae53d6430da',
+    redirectUri: 'http://localhost:4200',
+    scopes: ['api://3953353c-e525-4bcf-bc5b-8a3f2fef63dd/access_as_user']
   }
 };
