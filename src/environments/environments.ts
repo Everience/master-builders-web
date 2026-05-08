1182939c6204bc5e14dbaa09d9a96a62da8875c9
Everience Italy <114008603+Everience@users.noreply.github.com>
2026-05-08 17:39:11 +0200

    Update redirectUri to use window.location.origin

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -8,7 +8,7 @@ export const environment = {
   msal: {
     clientId: '767114fd-3d06-45b5-a2bb-7709837a92d9',
     tenantId: '456c3ca4-0074-406c-aefc-7ae53d6430da',
-    redirectUri: 'http://localhost:4200/login',
+    redirectUri: window.location.origin,
     scopes: ['api://3953353c-e525-4bcf-bc5b-8a3f2fef63dd/access_as_user']
   }
 };
